8c6827656b67c3168d1ad5aedc4dc964f80bdbcc
Upayan <upayansaha@icloud.com>
2020-11-25 22:26:57 +0530

    Minor edit to compile batch script

--- Gnome/compile64.bat
+++ Gnome/compile64.bat
@@ -2,9 +2,8 @@
 
 rc Src\\Resource.rc
 cvtres /MACHINE:x64 /OUT:Src\\Resource.o Src\\Resource.res
-cl.exe /nologo /MT /GS- /Od /DNDEBUG /W0 /Tp Src\\dllmain.cpp /link Src\\Resource.o Kernel32.lib ucrt.lib libvcruntime.lib libcmt.lib libcpmt.lib /DLL /NODEFAULTLIB /ENTRY:DllMain /OUT:Bin\\gnome_x64.dll /MACHINE:x64 /RELEASE /MERGE:_RDATA=.text /EMITPOGOPHASEINFO
+cl.exe /nologo /MT /GS- /Od /DNDEBUG /W0 /Tp Src\\dllmain.cpp /link Src\\Resource.o Kernel32.lib ucrt.lib libvcruntime.lib libcmt.lib libcpmt.lib /DLL /NODEFAULTLIB /ENTRY:DllMain /OUT:Bin\\gnome_x64.dll /MACHINE:x64 /RELEASE /Brepro /MERGE:_RDATA=.text /EMITPOGOPHASEINFO
 cd Python & python ConvertToShellcode.py ..\\Bin\gnome_x64.dll & cd ..
-copy Bin\gnome_x64.bin Z:\
 del dllmain.obj
 del Src\\Resource.o
 del Src\\Resource.res
